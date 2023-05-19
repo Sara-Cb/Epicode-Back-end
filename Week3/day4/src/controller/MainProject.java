aa5ac1f3616443a062c6387601b050b7d944e7e9
Sara-Cb <saramaiden23@gmail.com>
2023-05-19 12:53:57 +0200

    -

---
+++ Week3/day4/src/controller/MainProject.java
@@ -0,0 +1,16 @@
+package controller;
+
+import java.time.LocalDate;
+
+import model.Evento;
+import utils.tipoEvento;
+import dao.EventoDAO;
+
+
+public class MainProject {
+
+	public static void main(String[] args) {
+
+	}
+
+}
